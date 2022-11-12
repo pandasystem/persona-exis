87029b18955e1941d1cabb95e81d0fdd48f7830b
KBPsystem's-panda <96244940+pandasystem@users.noreply.github.com>
2022-11-12 19:33:05 +0800

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -4,20 +4,20 @@ pragma solidity ^0.8.4;
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LXStudioNFT is ERC721A, Ownable {
-    uint256 MAX_SUPPLY = 101714; // Initial max supply of NFTs
+contract PersonaExis is ERC721A, Ownable {
 
     // Using temporary metadata from MFERS' IPFS
     string public baseURI = "ipfs://bafybeieyetlp2c2vubffzjjap7utuz5jwo2k5b5kupvezfchc5tnfg4fh4/";
 
     constructor() ERC721A("Persona Exis NFT", "PXNFT") {}
 
-    function mint(uint256 quantity) external onlyOwner {
-        _safeMint(msg.sender, quantity);
+    function mint() external onlyOwner {
+        // @dev This will mint 1 NFT at a time
+        _safeMint(msg.sender, 1);
     }
 
 
-// Placeholder withdraw function - TODO
+    // Placeholder withdraw function
     function withdraw() external payable onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
